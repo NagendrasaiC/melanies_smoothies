f1c2924baa594a97699bb5e3a17add7eb9429fad
NagendrasaiC <162429195+NagendrasaiC@users.noreply.github.com>
2024-03-06 16:03:57 +0530

    Update streamlit_app.py
    added new dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,10 @@ if ingredients_list:
     
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit +' '
-        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
     # st.write(ingredients_string);
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -46,5 +49,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered! '+ title, icon="✅")
  
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
+
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
