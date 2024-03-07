42d345a9688fbffe8b97c04c4d567ce3485031c3
NagendrasaiC <162429195+NagendrasaiC@users.noreply.github.com>
2024-03-07 11:29:09 +0530

    Update streamlit_app.py
    added fruit search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -35,6 +35,10 @@ if ingredients_list:
     
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit +' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', each_fruit,' is ', search_on, '.')
+        
         st.subheader(each_fruit +'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
